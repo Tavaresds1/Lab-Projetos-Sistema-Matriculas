58905aae77c55fb3cd9006eee66437a848031f96
Vitor <vitorpereiramelo@hotmail.com>
2025-03-09 14:45:06 -0300

    update aluno

--- Code/Aluno.java
+++ Code/Aluno.java
@@ -1,21 +1,57 @@
-class Aluno extends Usuario {
+import java.util.List;
+
+class Aluno {
 
 	private String matricula;
 
-	private Disciplina[] disciplina;
+	private Usuario usuario;
 
-	private Disciplina[] disciplina;
+	private List<DisciplinaOptativa> disciplinasMatriculadasOptativa;
+	private List<DisciplinaObrigatoria> disciplinasMatriculadasObrigatoria;
 
-	public Aluno(String login, String senha, String matricula) {
+	public Aluno(String matricula, Usuario usuario) {
 
+		this.matricula = matricula;
+		this.usuario = usuario;
 	}
 
-	public void matricularDisciplinaObrigatoria(Disciplina disciplina) {
+	public void matricularDisciplinaObrigatoria(DisciplinaObrigatoria disciplina) {
 
+
+		if (disciplinasMatriculadasObrigatoria.size() < 6){
+			disciplinasMatriculadasObrigatoria.add(disciplina);
+		}
+		else{
+			System.out.println("Numero maximo de disciplinas obrigatorias atingido");
+		}
 	}
 
-	public void matricularDisciplinaOptativa(Disciplina disciplina) {
-
+	public void matricularDisciplinaOptativa(DisciplinaOptativa disciplina) {
+          if (disciplinasMatriculadasOptativa.size() < 2){
+			disciplinasMatriculadasOptativa.add(disciplina);
+		  }
+		else {
+			System.out.println("Numero maximo de disciplinas optativas atingido");
+		}
 	}
+
+	public void cancelarMatriculaDisciplinaObrigatoria(DisciplinaObrigatoria disciplina) {
+
+		disciplinasMatriculadasObrigatoria.remove(disciplina);
+	}
+
+	public void cancelarMatriculaDisciplinaOptativa(DisciplinaOptativa disciplina) {
+
+		disciplinasMatriculadasOptativa.remove(disciplina);
+	}
+
+
+
+
+	public boolean validarLogin(String login, String senha) {
+
+		return usuario.validarLogin(login, senha);
+	}
+
 
 }
